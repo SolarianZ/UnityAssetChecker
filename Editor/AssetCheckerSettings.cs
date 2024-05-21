b604674ea5f5cdfad3af375687ac898237eafde5
zqy <VDerGoW@hotmail.com>
2024-05-21 23:01:43 +0800

    feat: Asset checker profile

--- Editor/AssetCheckerSettings.cs
+++ Editor/AssetCheckerSettings.cs
@@ -25,5 +25,37 @@ namespace GBG.AssetChecking.Editor
 #endif
         [Tooltip("Provides checkers for checking assets. Required.")]
         public AssetChecker[] assetCheckers = Array.Empty<AssetChecker>();
+
+#if ODIN_INSPECTOR
+        [Required]
+        [RequiredListLength(1, null)]
+#endif
+        public AssetCheckerProfile[] profiles = Array.Empty<AssetCheckerProfile>();
+    }
+
+    [Serializable]
+    public class AssetCheckerProfile
+    {
+#if ODIN_INSPECTOR
+        [Required]
+#endif
+        [Tooltip("Name of the profile.")]
+        public string name = DateTime.Now.ToString();
+
+#nullable enable
+        [Tooltip("Provides custom views. Can be null.")]
+        public CustomViewProvider? customViewProvider;
+#nullable disable
+#if ODIN_INSPECTOR
+        [Required]
+#endif
+        [Tooltip("Provides assets for checking. Required.")]
+        public AssetProvider assetProvider;
+#if ODIN_INSPECTOR
+        [Required]
+        [RequiredListLength(1, null)]
+#endif
+        [Tooltip("Provides checkers for checking assets. Required.")]
+        public AssetChecker[] assetCheckers = Array.Empty<AssetChecker>();
     }
 }
